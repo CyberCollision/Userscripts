f982ada6b5f33088c81b0e3f1305d2ccdd6fe212
Cyber Collision <86217113+CyberCollision@users.noreply.github.com>
2023-06-24 09:16:32 -0500

    Update watchlist-enhancer.js

--- 9anime/watchlist-enhancer.js
+++ 9anime/watchlist-enhancer.js
@@ -6,7 +6,7 @@
 // @match       *://9anime.gs/user/watch-list
 // @match       *://9anime.id/user/watch-list
 // @icon        https://www.google.com/s2/favicons?domain=https://9anime.me
-// @version     2.1.0
+// @version     3.0.0
 // @author      CyberCollision
 // @description This userscript enhances the appearance of the watchlist on 9anime
 // @license     MIT
@@ -16,11 +16,15 @@
   'use strict';
 
   // Settings
-  const imageSize = '200px'; // Set the desired image size here
+  const imageSize = '180px'; // Set the desired image size here
+  const paddingValue = '0 10px 160px'; // Set the desired padding value here
 
   const items = document.querySelectorAll('.watchlist.scaff.items .item');
 
   items.forEach(item => {
+    const info = item.querySelector('.info');
+    info.style.padding = paddingValue;
+
     const poster = item.querySelector('.poster');
     const imgElement = poster.querySelector('span img');
     let imgSrc = imgElement.getAttribute('src');
@@ -28,9 +32,11 @@
     imgElement.setAttribute('src', imgSrc);
     imgElement.style.width = imageSize;
     poster.style.width = imageSize;
+
     const titleLink = item.querySelector('.name a');
     titleLink.style.fontSize = '1.3rem';
-    titleLink.style.display = 'initial';
+    titleLink.style.display = 'inline';
+
     const detail = item.querySelector('.info .detail');
     detail.style.fontSize = '1rem';
   });
