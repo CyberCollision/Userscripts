63ddc45b48bd789ccc5bb8fe5ac2d015ed1a9545
Cyber Collision <86217113+CyberCollision@users.noreply.github.com>
2023-06-24 08:51:37 -0500

    Update watchlist-enhancer.js

--- 9anime/watchlist-enhancer.js
+++ 9anime/watchlist-enhancer.js
@@ -1,44 +1,37 @@
 // ==UserScript==
-// @name        Watchlist Enhancer
+// @name        Enhanced Watchlist
 // @namespace   https://github.com/CyberCollision/Userscripts
 // @match       *://9anime.to/user/watch-list
 // @match       *://9anime.pl/user/watch-list
 // @match       *://9anime.gs/user/watch-list
 // @match       *://9anime.id/user/watch-list
 // @icon        https://www.google.com/s2/favicons?domain=https://9anime.me
-// @version     2.0.3
+// @version     2.1.0
 // @author      CyberCollision
 // @description This userscript enhances the appearance of the watchlist on 9anime
 // @license     MIT
 // ==/UserScript==
 
 (function() {
-    'use strict';
+  'use strict';
 
-    // Get all the items with class 'item' inside the div with class 'watchlist scaff items'
-    const items = document.querySelectorAll('.watchlist.scaff.items .item');
+  // Settings
+  const imageSize = '200px'; // Set the desired image size here
 
-    // Loop through each item
-    items.forEach(item => {
-        // Find the poster image inside the item
-        const poster = item.querySelector('.poster');
+  const items = document.querySelectorAll('.watchlist.scaff.items .item');
 
-        // Change the width of the poster image to 100px
-        poster.style.width = '100px';
-
-        // Find the title link inside the item
-        const titleLink = item.querySelector('.name a');
-
-        // Change the font size of the title link to 1.3rem
-        titleLink.style.fontSize = '1.3rem';
-
-        // Reset the display property of the title link
-        titleLink.style.display = 'initial';
-
-        // Find the detail section inside the item
-        const detail = item.querySelector('.info .detail');
-
-        // Change the font size of the detail section to 1rem
-        detail.style.fontSize = '1rem';
-    });
+  items.forEach(item => {
+    const poster = item.querySelector('.poster');
+    const imgElement = poster.querySelector('span img');
+    let imgSrc = imgElement.getAttribute('src');
+    imgSrc = imgSrc.replace(/-w100$/, '');
+    imgElement.setAttribute('src', imgSrc);
+    imgElement.style.width = imageSize;
+    poster.style.width = imageSize;
+    const titleLink = item.querySelector('.name a');
+    titleLink.style.fontSize = '1.3rem';
+    titleLink.style.display = 'initial';
+    const detail = item.querySelector('.info .detail');
+    detail.style.fontSize = '1rem';
+  });
 })();
